ea80c8f79ca66eedb62751c1fd2f029c58f22d0b
Luis Fraguada <fraguada@gmail.com>
2021-01-16 14:29:15 +0100

    adding second example

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # lecture2
+
+Examples for Lecture 2
+
+These examples introduce html and javascript concepts we'll be using in the seminar.
+
+- [Example 1](example1): A sample that uses the [three.js](https://threejs.org) library
+- [Example 2](example2): A sample that uses the [rhino3dm.js]() library
+
+## References
+
+- [Three.js Getting Started](https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene)
+- [rhino3dm.js API reference](https://mcneel.github.io/rhino3dm/javascript/api/index.html)
+- [rhino3dm developer samples](https://github.com/mcneel/rhino-developer-samples/tree/7/rhino3dm)
---
+++ example2/script.js
@@ -0,0 +1,43 @@
+// Import libraries
+import * as THREE from 'https://unpkg.com/three@0.124.0/build/three.module.js'
+import { OrbitControls } from 'https://unpkg.com/three@0.124.0/examples/jsm/controls/OrbitControls.js'
+import { Rhino3dmLoader } from 'https://unpkg.com/three@0.124.0/examples/jsm/loaders/3DMLoader.js'
+
+THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
+
+// create a scene and a camera
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+camera.position.y = - 30
+
+// create the renderer and add it to the html
+const renderer = new THREE.WebGLRenderer( { antialias: true } )
+renderer.setSize( window.innerWidth, window.innerHeight )
+document.body.appendChild( renderer.domElement )
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+const directionalLight = new THREE.DirectionalLight( 0xffffff );
+directionalLight.position.set( 0, 0, 2 );
+directionalLight.castShadow = true;
+directionalLight.intensity = 2;
+scene.add( directionalLight );
+
+const loader = new Rhino3dmLoader()
+loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.13.0/' )
+
+loader.load( 'Rhino_Logo.3dm', function ( object ) {
+
+    scene.add( object )
+
+} )
+
+function animate() {
+
+    requestAnimationFrame( animate )
+    renderer.render( scene, camera )
+
+}
+
+animate()
+
