0749b86fa422e3eb7a6902ab51be43c522111794
Luis Fraguada <fraguada@gmail.com>
2021-01-18 00:11:55 +0100

    adding example 3

--- example3/script.js
+++ example3/script.js
@@ -3,100 +3,100 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.124.0/build/three.m
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/controls/OrbitControls.js'
 import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/loaders/3DMLoader.js'
 
-const button = document.querySelector('input')
-button.addEventListener('click', download)
+let camera, scene, raycaster, renderer
+const mouse = new THREE.Vector2()
+window.addEventListener( 'click', onClick, false);
 
-// ensure the rhino3dm library is loaded
-let rhino
-rhino3dm().then(async m => {
-    console.log( 'Loaded rhino3dm.' )
-    rhino = m // global
+init()
+animate()
 
-    // now that the library is loaded, initialize three.js scene, etc and create ojects
-    init()
-    create()
-})
+function init() {
 
-let doc
-function create () {
-
-    // create a new Rhino document
-    doc = new rhino.File3dm()
-
-    // define a few points
-    const ptA = [0, 0, 0]
-    const ptB = [10, 10, 10]
-
-    // add a point to the document
-    doc.objects().addPoint(ptA)
-
-    // create a line and add it to the document
-    const line = new rhino.LineCurve( ptA, ptB )
-    doc.objects().add(line, null)
-
-    // create a circle and add it to the document
-    const circle = new rhino.Circle( 10 )
-    circle.center = ptB
-    doc.objects().add(circle.toNurbsCurve(), null)
-
-    // set up Rhino loader 
-    const loader = new Rhino3dmLoader()
-    loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.13.0/' )
-
-    // create a copy of the doc.toByteArray data to get an ArrayBuffer
-    let arr = new Uint8Array( doc.toByteArray() ).buffer
-
-    loader.parse( arr, function ( object ) {
-
-      // hide spinner
-      document.getElementById('loader').remove()
-      button.disabled = false
-      console.log(object)
-      scene.add(object)
-  
-    } )
-
-}
-
-function download() {
-
-    const buffer = doc.toByteArray()
-    const blob = new Blob( [ buffer ], { type: 'application/octect-stream' } )
-    const link = document.createElement( 'a' )
-    link.href = window.URL.createObjectURL( blob )
-    link.download = 'myRhinoFile.3dm'
-    link.click()
-
-}
-
-let scene, camera, renderer
-
-function init () {
+    THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 )
 
     // create a scene and a camera
     scene = new THREE.Scene()
     scene.background = new THREE.Color(1,1,1)
-
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
-    camera.position.z = - 30
+    camera.position.y = - 30
 
     // create the renderer and add it to the html
     renderer = new THREE.WebGLRenderer( { antialias: true } )
     renderer.setSize( window.innerWidth, window.innerHeight )
     document.body.appendChild( renderer.domElement )
 
-    const controls = new OrbitControls( camera, renderer.domElement );
+    const controls = new OrbitControls( camera, renderer.domElement )
 
-    animate()
+    const directionalLight = new THREE.DirectionalLight( 0xffffff )
+    directionalLight.position.set( 0, 0, 2 )
+    directionalLight.castShadow = true
+    directionalLight.intensity = 2
+    scene.add( directionalLight )
+
+    raycaster = new THREE.Raycaster()
+
+    const loader = new Rhino3dmLoader()
+    loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.13.0/' )
+
+    loader.load( 'sphere.3dm', function ( object ) {
+
+        document.getElementById('loader').remove()
+        scene.add( object )
+        console.log( object )
+
+    } )
 
 }
 
-const animate = function () {
+function onClick( event ) {
+
+    console.log( 'clicked!' )
+
+	// calculate mouse position in normalized device coordinates
+    // (-1 to +1) for both components
+
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    
+    raycaster.setFromCamera( mouse, camera )
+
+	// calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects( scene.children, true )
+
+    let container = document.getElementById( 'container' )
+    if (container) container.remove()
+
+    
+    if (intersects.length > 0) {
+
+        const cnt = intersects[0].object.parent.userData.attributes.userStringCount
+
+        if ( cnt === 0 ) return
+
+        const data = intersects[0].object.parent.userData.attributes.userStrings
+
+        container = document.createElement( 'div' )
+        container.id = 'container'
+
+        for ( let i = 0; i < data.length; i ++ ) {
+
+            let entry = document.createElement( 'div' )
+            entry.innerHTML = data[ i ][ 0 ] + ': ' + data[ i ][ 1 ]
+
+            container.appendChild( entry )
+        }
+
+        document.body.appendChild( container )
+
+        console.log(intersects[0].object.parent.userData.attributes.userStrings)
+    }
+
+}
+
+function animate() {
 
     requestAnimationFrame( animate )
     renderer.render( scene, camera )
 
 }
-
-
 
