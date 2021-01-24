86af6bff32452819029a2402606a1191c8aacb39
nawapans <nawapan.suntorachai@students.iaac.net>
2021-01-24 22:48:23 +0700

    Assignment 2 - Lecture 2 - Nawapan
    @pearswj @fraguada
    Hi, I have create an assignment 2 based on example 3- with further study on threejs

--- assognment2/script.js
+++ assognment2/script.js
@@ -19,7 +19,7 @@ function init() {
     scene.background = new THREE.Color(1, 1, 1)
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 5, 1000)
-    camera.position.y = -100
+    camera.position.set(0, 30, 0);
 
     // create the renderer and add it to the html
     renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -29,25 +29,49 @@ function init() {
     const controls = new OrbitControls(camera, renderer.domElement)
 
     const directionalLight = new THREE.DirectionalLight(0xffffff)
-    directionalLight.position.set(0, 0, 2)
+    directionalLight.position.set(20, 50, 22)
     directionalLight.castShadow = true
     directionalLight.intensity = 2
     scene.add(directionalLight)
+
+    const directionalLight2 = new THREE.DirectionalLight(0xffffff)
+    directionalLight2.position.set(-20, -50, 22)
+    directionalLight2.castShadow = true
+    directionalLight2.intensity = 2
+    scene.add(directionalLight2)
 
     raycaster = new THREE.Raycaster()
 
     const loader = new Rhino3dmLoader()
     loader.setLibraryPath('https://cdn.jsdelivr.net/npm/rhino3dm@0.13.0/')
 
-    loader.load('sphere.3dm', function(object) {
+    loader.load('model.3dm', function(object) {
 
         document.getElementById('loader').remove()
         scene.add(object)
         console.log(object)
 
+
+
+        let container = document.getElementById('container')
+        container = document.createElement('div')
+        container.id = 'container'
+        const table = document.createElement('table')
+        container.appendChild(table)
+        const row = document.createElement('tr')
+        row.innerHTML = `<td>What is in the boxes?</td>`
+        table.appendChild(row)
+        document.body.appendChild(container)
+
+
     })
 
+
+
+
 }
+
+
 
 function onClick(event) {
 
@@ -65,34 +89,37 @@ function onClick(event) {
     const intersects = raycaster.intersectObjects(scene.children, true)
 
     let container = document.getElementById('container')
-    if (container) container.remove()
-
-    // reset object colours
-    scene.traverse((child, i) => {
-        if (child.isMesh) {
-            child.material.color.set('white')
-        }
-    });
+    if (container) {
+        container.remove()
+    }
 
     if (intersects.length > 0) {
-
         // get closest object
         const object = intersects[0].object
-        console.log(object) // debug
 
-        object.material.color.set('yellow')
+        // create transparency of materials when clicked
+        console.log(object) // debug
+        object.material.color.set('pink')
+            //object.material.opacity = 0.2
+            //object.material.transparent = true
+        object.rotation.x += 0.02;
+        object.rotation.y += 0.02;
+        object.rotation.z += 0.1;
 
         // get user strings
         let data, count
         if (object.userData.attributes !== undefined) {
             data = object.userData.attributes.userStrings
+            object.material.color.set('crimson')
         } else {
             // breps store user strings differently...
             data = object.parent.userData.attributes.userStrings
         }
 
         // do nothing if no user strings
-        if (data === undefined) return
+        if (data === undefined)
+
+            return
 
         console.log(data)
 
@@ -103,12 +130,16 @@ function onClick(event) {
         const table = document.createElement('table')
         container.appendChild(table)
 
+
+
         for (let i = 0; i < data.length; i++) {
 
             const row = document.createElement('tr')
-            row.innerHTML = `<td>${data[ i ][ 0 ]}</td><td>${data[ i ][ 1 ]}</td>`
+            row.innerHTML = `<td>Click Click, and more Click</td>`
+                //row.innerHTML = `<td>Inside this box there is ${data[ i ][ 0 ]}</td><td>${data[ i ][ 1 ]}</td>`
             table.appendChild(row)
         }
+
 
         document.body.appendChild(container)
     }
@@ -119,6 +150,8 @@ function animate() {
 
 
     requestAnimationFrame(animate)
+
     renderer.render(scene, camera)
+
 
 }
