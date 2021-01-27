b423a98980be8c6b480e4441b3e2801249f499b0
keshavanarayan <74804559+keshavanarayan@users.noreply.github.com>
2021-01-27 13:29:48 +0530

    Final

--- example5/script.js
+++ example5/script.js
@@ -12,112 +12,104 @@ animate()
 
 function init() {
 
-    THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 )
+    THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
 
-    // create a scene and a camera
-    scene = new THREE.Scene()
-    scene.background = new THREE.Color(1,1,1)
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
-    camera.position.y = - 100
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.set( 26, - 40, 5 );
 
-    // create the renderer and add it to the html
-    renderer = new THREE.WebGLRenderer( { antialias: true } )
-    renderer.setSize( window.innerWidth, window.innerHeight )
-    document.body.appendChild( renderer.domElement )
+    scene = new THREE.Scene();
 
-    const controls = new OrbitControls( camera, renderer.domElement )
+    const directionalLight = new THREE.DirectionalLight( 0xffffff );
+    directionalLight.position.set( 0, 0, 2 );
+    directionalLight.castShadow = true;
+    directionalLight.intensity = 2;
+    scene.add( directionalLight );
 
-    const directionalLight = new THREE.DirectionalLight( 0xffffff )
-    directionalLight.position.set( 0, 0, 2 )
-    directionalLight.castShadow = true
-    directionalLight.intensity = 2
-    scene.add( directionalLight )
+// change according to old docs
+    const loader = new Rhino3dmLoader();
+    loader.setLibraryPath( 'jsm/libs/rhino3dm/' );
 
-    raycaster = new THREE.Raycaster()
+    loader.load( 'models/3dm/Rhino_Logo.3dm', function ( object ) {
 
-    const loader = new Rhino3dmLoader()
-    loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.13.0/' )
+        scene.add( object );
+        initGUI( object.userData.layers );
 
-    loader.load( 'For Krishna Final model.3dm', function ( object ) {
+    } );
 
-        document.getElementById('loader').remove()
-        scene.add( object )
-        console.log( object )
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-    } )
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( width, height );
+    container.appendChild( renderer.domElement );
+
+    controls = new OrbitControls( camera, container );
+
+    window.addEventListener( 'resize', resize, false );
 
 }
 
-function onClick( event ) {
+function resize() {
 
-    console.log( `click! (${event.clientX}, ${event.clientY})`)
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-	// calculate mouse position in normalized device coordinates
-    // (-1 to +1) for both components
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
 
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-    
-    raycaster.setFromCamera( mouse, camera )
-
-	// calculate objects intersecting the picking ray
-    const intersects = raycaster.intersectObjects( scene.children, true )
-
-    let container = document.getElementById( 'container' )
-    if (container) container.remove()
-
-    // reset object colours
-    scene.traverse((child, i) => {
-        if (child.isMesh) {
-            child.material.color.set( 'white' )
-        }
-    });
-
-    if (intersects.length > 0) {
-
-        // get closest object
-        const object = intersects[0].object
-        console.log(object) // debug
-
-        object.material.color.set( 'yellow' )
-
-        // get user strings
-        let data, count
-        if (object.userData.attributes !== undefined) {
-            data = object.userData.attributes.userStrings
-        } else {
-            // breps store user strings differently...
-            data = object.parent.userData.attributes.userStrings
-        }
-
-        // do nothing if no user strings
-        if ( data === undefined ) return
-
-        console.log( data )
-        
-        // create container div with table inside
-        container = document.createElement( 'div' )
-        container.id = 'container'
-        
-        const table = document.createElement( 'table' )
-        container.appendChild( table )
-
-        for ( let i = 0; i < data.length; i ++ ) {
-
-            const row = document.createElement( 'tr' )
-            row.innerHTML = `<td>${data[ i ][ 0 ]}</td><td>${data[ i ][ 1 ]}</td>`
-            table.appendChild( row )
-        }
-
-        document.body.appendChild( container )
-    }
+    renderer.setSize( width, height );
 
 }
 
 function animate() {
 
-    requestAnimationFrame( animate )
-    renderer.render( scene, camera )
+    controls.update();
+    renderer.render( scene, camera );
+
+    requestAnimationFrame( animate );
+
+}
+
+function initGUI( layers ) {
+
+    gui = new GUI( { width: 300 } );
+    const layersControl = gui.addFolder( 'layers' );
+    layersControl.open();
+
+    for ( let i = 0; i < layers.length; i ++ ) {
+
+        const layer = layers[ i ];
+        layersControl.add( layer, 'visible' ).name( layer.name ).onChange( function ( val ) {
+
+            const name = this.object.name;
+
+            scene.traverse( function ( child ) {
+
+                if ( child.userData.hasOwnProperty( 'attributes' ) ) {
+
+                    if ( 'layerIndex' in child.userData.attributes ) {
+
+                        const layerName = layers[ child.userData.attributes.layerIndex ].name;
+
+                        if ( layerName === name ) {
+
+                            child.visible = val;
+                            layer.visible = val;
+
+                        }
+
+                    }
+
+                }
+
+            } );
+
+        } );
+
+    }
 
 }
 
